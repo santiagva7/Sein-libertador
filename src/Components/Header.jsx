2e2c50d9bf1a3309a0dbd83f0d04cc3f570130c0
Santiagv7 <116013204+Santiagv7@users.noreply.github.com>
2023-11-07 22:31:18 -0300

    size adjustment

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -9,23 +9,23 @@ const Header = () => {
   return (
     <header className="bg-p1 border-gray-300 flex border-b border-p8">
       <div className="h-full flex items-center justify-center">
-        <Image src="/iconoPng.png" width={100} height={100} alt="Icono distintivo de la empresa Sein, de los hermanos Hengen" />
+        <Image src="/iconoPng.png" width={75} height={75} alt="Icono distintivo de la empresa Sein, de los hermanos Hengen" />
       </div>
       <div className="max-w-2xl mx-auto font-mono text-center  backdrop-blur-md">
         <div className="pb-2 border-r border-l border-p3 shadow-2xl">
-        <h1 className="pt-7 font-sans font-bold text-logo border-p1 text-6xl">SEIN </h1>
-        <p className="text-logo text-center text-lg"> Libertador</p>
+        <h1 className="pt-7 font-sans font-bold text-logo border-p1 text-4xl">SEIN </h1>
+        <p className="text-logo text-center text-base"> Libertador</p>
         </div>
       </div>
       <div className="flex">
         <div className="h-full flex items-center justify-center">
-          <a href="https://wa.me/3329676205"><FaWhatsapp className="text-3xl text-p4 m-4 shadow-lg" /></a>
+          <a href="https://wa.me/3329676205"><FaWhatsapp className="text-xl text-p4 m-4 shadow-lg" /></a>
         </div>
         <div className="h-full flex items-center justify-center">
-          <a href="mailto:hengenotoniel@gmail.com"><FiMail className="text-3xl text-p4 m-4 shadow-lg" /></a>
+          <a href="mailto:hengenotoniel@gmail.com"><FiMail className="text-xl text-p4 m-4 shadow-lg" /></a>
         </div>
         <div className="h-full flex items-center justify-center">
-          <a href={instagram}><FaInstagram className="text-3xl text-p4 m-4 shadow-lg" /></a>
+          <a href={instagram}><FaInstagram className="text-xl text-p4 m-4 shadow-lg" /></a>
         </div>
       </div>
     </header>
